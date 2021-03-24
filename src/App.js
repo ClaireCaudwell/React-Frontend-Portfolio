0d2a1817b27513774146f02deb1bd9c09951bc94
ClaireCaudwell <clairecaudwell@gmail.com>
2021-03-24 11:48:16 +0100

    Added some images and started a data file for my projects

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
-import React from 'react'
+import React from 'react';
+
+import { Header } from "./components/header.js";
 
 export const App = () => {
   return (
-    <div>
-      Find me in src/app.js!
-    </div>
-  )
-}
+    <>
+      <Header />
+    </>
+  );
+};
